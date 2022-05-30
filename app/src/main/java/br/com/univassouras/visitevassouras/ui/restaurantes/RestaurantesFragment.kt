9cfb49bb0597c79d32b30dbc51a4b267f598ec7b
gabsgc <gabriellagarccia@gmail.com>
2022-05-30 19:00:11 -0300

    GET Hoteis and update layouts

--- app/src/main/java/br/com/univassouras/visitevassouras/ui/restaurantes/RestaurantesFragment.kt
+++ app/src/main/java/br/com/univassouras/visitevassouras/ui/restaurantes/RestaurantesFragment.kt
@@ -1,20 +1,11 @@
 package br.com.univassouras.visitevassouras.ui.restaurantes
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import br.com.univassouras.visitevassouras.databinding.FragmentRestaurantesBinding
 import br.com.univassouras.visitevassouras.retrofit.RetrofitInstance
 import kotlinx.coroutines.Dispatchers
