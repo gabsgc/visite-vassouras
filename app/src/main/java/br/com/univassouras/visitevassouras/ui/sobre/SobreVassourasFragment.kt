6c5b3ed8281769e0ba1fb363c5c6f3c5f9de93f5
gabsgc <gabriellagarccia@gmail.com>
2022-05-31 12:27:26 -0300

    Add information in SobreVassourasFragment and update layout

--- app/src/main/java/br/com/univassouras/visitevassouras/ui/sobre/SobreVassourasFragment.kt
+++ app/src/main/java/br/com/univassouras/visitevassouras/ui/sobre/SobreVassourasFragment.kt
@@ -1,15 +1,18 @@
 package br.com.univassouras.visitevassouras.ui.sobre
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import br.com.univassouras.visitevassouras.R
 import br.com.univassouras.visitevassouras.databinding.FragmentSobreVassourasBinding
 
 class SobreVassourasFragment : Fragment() {
-    private var _binding: FragmentSobreVassourasBinding? = null
-    private val binding get() = _binding!!
+    private var binding: FragmentSobreVassourasBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,14 +20,26 @@ class SobreVassourasFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSobreVassourasBinding.inflate(inflater, container,false)
-        val root: View = binding.root
+        binding = FragmentSobreVassourasBinding.inflate(inflater, container,false)
+        val root: View = binding!!.root
+
+        binding!!.tvSobreCidadeHistorica.text = getString(R.string.sobre_historica)
+        binding!!.tvSobreCidadeTuristica.text = getString(R.string.sobre_turistica)
+        binding!!.tvSobreCidadeUniversitaria.text = getString(R.string.sobre_universitaria)
+
+        binding!!.mbQueroVisitarVassouras.setOnClickListener {
+            val url = "https://goo.gl/maps/xfr7KVRxtoKE1cpKA"
+            val uri = url.toUri()
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse(uri.toString())
+            startActivity(openURL)
+        }
 
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
