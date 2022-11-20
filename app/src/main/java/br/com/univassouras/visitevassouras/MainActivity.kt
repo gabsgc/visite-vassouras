cb4e84ea50c6f410042ed23b6dbd0ff83b0fd337
gabsgc <gabriellagarccia@gmail.com>
2022-11-20 16:26:47 -0300

    add behavior for no-connection scenarios

--- app/src/main/java/br/com/univassouras/visitevassouras/MainActivity.kt
+++ app/src/main/java/br/com/univassouras/visitevassouras/MainActivity.kt
@@ -1,11 +1,16 @@
 package br.com.univassouras.visitevassouras
 
+import android.R
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import br.com.univassouras.visitevassouras.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +19,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        loadSplashScreen()
+        if(!isNetworkAvailable(this)) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                AlertDialog.Builder(this)
+                    .setTitle("Sem conexão com a internet")
+                    .setMessage("Por favor, verifique sua conexão e tente novamente.")
+                    .setPositiveButton("OK") { _, _ ->
+                        finish()
+                    }
+                    .setIcon(R.drawable.ic_dialog_alert)
+                    .show()
+            }, 3000)
+        } else {
+            loadSplashScreen()
+        }
+
     }
 
     private fun loadSplashScreen() {
@@ -24,4 +43,10 @@ class MainActivity : AppCompatActivity() {
             finish()
         }, 2000)
     }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        val conMan = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        return conMan.activeNetworkInfo != null && conMan.activeNetworkInfo!!.isConnected
+    }
+
 }
